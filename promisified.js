71782300d1a52dd5d1294249d63647ac173240de
Maxim <cvlt.mao@gmail.com>
2017-11-13 18:51:31 +0500

    Tested version; chaingear.json constructor moved to upvoter.js

--- promisified.js
+++ promisified.js
@@ -1,4 +1,18 @@
 const request = require('request')
+const GitHubApi = require("github")
+const config = require('../config.json')
+const base64 = require('base-64')
+const utf8 = require('utf8')
+
+const github = new GitHubApi({
+    debug: false
+  })
+
+github.authenticate({
+  type: "basic",
+  username: "ninjascant",
+  password: config.git_key
+})
 
 const readFile = (repository) => {
   return new Promise((resolve, reject) => {
@@ -11,6 +25,31 @@ const apiReq = (url, options) => {
       (err, response, body) => err?reject(err):resolve(JSON.parse(body)))
   })
 }
+const apiPostReq = (url) => {
+  return new Promise((resolve, reject) => {
+    request.post(url, options,
+      (err, response, body) => err?reject(err):resolve(JSON.parse(body)))
+  })
+}
+const br = 'confideal'
+  const getRef = (owner, repo, ref) => {
+    return new Promise((resolve, reject) => {
+      github.gitdata.getReference({owner: 'ninjascant', repo: 'chaingear', ref: `heads/${br}`}, 
+        (err, res) => err?reject(err):resolve(res))
+    })
+  }
+  const getTree = (owner, repo, sha) => {
+    return new Promise((resolve, reject) => {
+      github.gitdata.getTree({owner: 'ninjascant', repo: 'chaingear', sha: sha}, 
+        (err, res) => err?reject(err):resolve(res))
+    })
+  }
+  const getBlob = (owner, repo, sha) => {
+    return new Promise((resolve, reject) => {
+      github.gitdata.getBlob({owner: 'ninjascant', repo: 'chaingear', sha: sha}, 
+        (err, res) => err?reject(err):resolve(res))
+    })
+  }
 const updateFile = (owner, repo, path, message, content, sha, branch) => {
   return new Promise((resolve, reject) => {
     github.repos.updateFile({
@@ -26,5 +65,9 @@ const updateFile = (owner, repo, path, message, content, sha, branch) => {
 }
 module.exports = {
   readFile: readFile,
-  apiReq: apiReq
+  apiReq: apiReq,
+  updateFile: updateFile,
+  getRef: getRef,
+  getTree: getTree,
+  getBlob: getBlob
 }
--- upvoter.js
+++ upvoter.js
@@ -6,6 +6,8 @@ const config = require('../config.json')
 const jsonfile = require('jsonfile')
 const _ = require('lodash')
 const prms = require('./promisified.js')
+const atob = require('atob')
+const toml = require('toml')
 
 const github = new Github({
   username: "goloschaingear",
@@ -13,7 +15,7 @@ const github = new Github({
   auth: "basic"
 })
 const username = "ninjascant",
-    password = "config.git_key"
+    password = config.git_key
 
 const options = {
   headers: {
@@ -25,6 +27,7 @@ const dataRepo = github.getRepo('goloschaingear', 'data')
 
 const port = 8080
 const path = '/'
+const br = 'confideal'
 const handler = createHandler({path: path, secret: config.secret})
 
 http.createServer((req, res) => {
@@ -38,62 +41,68 @@ handler.on('error', err => {
 	console.error('Error', err.message)
 })
 handler.on('push', event => {
-  //console.log(event)
-  const commits = event.payload.commits
+  console.log('Push event')
+  const commits = event.payload.commits.filter(commit => commit.added.length!==0 || commit.modified.length !== 0)
+  
+  if(commits.length === 0) return
   const blobGetUrl = 'https://api.github.com/repos/ninjascant/golos-academy/git/blobs/'
-  let updated = []
-
-  const promiseList = commits.map((commit) => {
-    if(commit.commit.commiter==='GolosBot') {
-      const projectName = commit.commit.message.split(/Add: /)[1]
-      const url = 'https://api.github.com/repos/goloschaingear/data/contents/bot-list.json'
-      apiReq(url, options)
-        .then(list => {
-          const projects = list.map(item => item.project)
-          return golos.broadcast.vote(config.wif,
-            'cyberanalytics',
-            projects[n].author,
-            projects[n].url.split('/')[2],
-            100,
-            (err, res) => console.log(err?err:'Upvoted!'))
-          })
-      .catch(error => console.log(error))
-    }
-    return commit.files.map(file=>{
-      if(file.status === 'modified' && file.filename !== 'chaingear') updated.push(file.filename)
-      if(file.filename === 'chaingear.json') return null
-      else return apiReq(blobGetUrl+file.sha)
-    }).filter(prom => prom !== null)
-  }).reduce((prev, curr) => prev.concat(curr))
+  const url = 'https://api.github.com/repos/ninjascant/golos-academy/commits/'
+  const promiseList = commits.map(commit => prms.apiReq((url + commit.id), options))
   Promise.all(promiseList)
-    .then(blobs => {
-      const files = blobs.map(blob => JSON.parse(Buffer.from(blob.content, 'base64').toString('utf8')))
-      const updated = files.map(file => file.system)
-      const chaingear = jsonfile.readFileSync('chaingear.json').filter(proj => updated.indexOf(proj.system)!==-1)
-      const n = chaingear.length
-      const fiat = chaingear.slice(n-15)
-      let crypto = chaingear.slice(0, n-15)
-      crypto = crypto.concat(files)
-      crypto = _.sortBy(crypto, ['system'])
-      const newFile = crypto.concat(fiat)
-      return jsonfile.writeFileSync('chaingear.json', newFile)
+    .then(commits => {
+      const promiseList = commits.map(commit => {
+        if(commit.commit.commiter==='GolosBot') {
+          const projectName = commit.commit.message.split(/Add: /)[1]
+          const url = 'https://api.github.com/repos/goloschaingear/data/contents/bot-list.json'
+          apiReq(url, options)
+            .then(list => {
+              const projects = list.map(item => item.project)
+              return golos.broadcast.vote(config.wif,
+                'cyberanalytics',
+                projects[n].author,
+                projects[n].url.split('/')[2],
+                100,
+                (err, res) => console.log(err?err:'Upvoted!'))
+              })
+          .catch(error => console.log(error))
+        }
+        return commit.files.map(file=>{
+          //if(file.status === 'modified' && file.filename !== 'chaingear') updated.push(file.filename)
+          if(file.filename === 'chaingear.json') return null
+          else return prms.apiReq(blobGetUrl+file.sha, options)
+        }).filter(prom => prom !== null)
+      }).reduce((prev, curr) => prev.concat(curr))
+      return Promise.all(promiseList)
     })
-    .then(none => {
-      return getRef('ninjascant', 'chaingear', br)
+  .then(blobs => {
+    const files = blobs.map(blob => toml.parse(atob(blob.content)))
+    //console.log(files)
+    const updated = files.map(file => file.system)
+    const chaingear = jsonfile.readFileSync('chaingear.json').filter(proj => updated.indexOf(proj.system)===-1)
+    const n = chaingear.length
+    const fiat = chaingear.slice(n-15)
+    let crypto = chaingear.slice(0, n-15)
+    crypto = crypto.concat(files)
+    crypto = _.sortBy(crypto, ['system'])
+    const newFile = crypto.concat(fiat)
+    return jsonfile.writeFileSync('chaingear.json', newFile, {spaces: 4})
+  })
+  .then(none => {
+      return prms.getRef('ninjascant', 'chaingear', br)
     })
     .then(res => {
       const sha = res.data.object.sha
-      return getTree('ninjascant', 'chaingear', sha)
+      return prms.getTree('ninjascant', 'chaingear', sha)
     })
     .then(res => {
       const sha = res.data.tree.filter(item => item.path==='chaingear.json')[0].sha
-      return getBlob('ninjascant', 'chaingear', sha)
+      return prms.getBlob('ninjascant', 'chaingear', sha)
     })
     .then(res => {
       const sha = res.data.sha
       const fileStr = JSON.stringify(jsonfile.readFileSync('chaingear.json', 'utf-8'), null, 4)
-      return updateFile('ninjascant', 'chaingear', 'chaingear.json', 'Commit from constructor.js', fileStr, sha, br)
+      return prms.updateFile('ninjascant', 'chaingear', 'chaingear.json', 'Commit from constructor.js', fileStr, sha, br)
     })
     .then(none => console.log(`File chaingear.json constructed and commited`))
-    .catch(error=>console.error(error))
+  .catch(error=>console.log(error))
 })
